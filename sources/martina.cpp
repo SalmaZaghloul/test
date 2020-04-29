bd85725ede68ac929a8cb15c9337e7fcf2ecf29f
martinamichelcx <martinamichelcx@gmail.com>
2020-04-29 21:54:01 +0200

    message

--- sources/martina.cpp
+++ sources/martina.cpp
@@ -1,4 +1,4 @@
-#include"/home/martinamichelcx/test/headers/martina.h"
+#include"/headers/martina.h"
 void martinamichelcx(){
 	printf("Martina Michel Muawad\n");
 	printf("martinamichelcx@gmail.com\n");
